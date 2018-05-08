d60b9f323c7b64a9c26ca49e89f1d6d830a85860
HEKINGSS <773874852@qq.com>
2018-05-08 16:21:39 +0800

    Revert "3D应急演练系统 0.1.5"
    This reverts commit 855f29b00985a2f3814bf87b38b64898d5110cff.

--- Assets/HQ_ResidentialHouse/DemoScene/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook_custom.cs
+++ Assets/HQ_ResidentialHouse/DemoScene/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook_custom.cs
@@ -50,9 +50,56 @@ namespace UnityStandardAssets.Characters.FirstPerson
                 character.localRotation = m_CharacterTargetRot;
                 camera.localRotation = m_CameraTargetRot;
             }
+<<<<<<< HEAD:Assets/HQ_ResidentialHouse/DemoScene/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook_custom.cs
         }
 
 
+=======
+
+            UpdateCursorLock();
+        }
+
+        public void SetCursorLock(bool value)
+        {
+            lockCursor = value;
+            if(!lockCursor)
+            {//we force unlock the cursor if the user disable the cursor locking helper
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        public void UpdateCursorLock()
+        {
+            //if the user set "lockCursor" we check & properly lock the cursos
+            if (lockCursor)
+                InternalLockUpdate();
+        }
+
+        private void InternalLockUpdate()
+        {
+            if(Input.GetKeyUp(KeyCode.Escape))
+            {
+                m_cursorIsLocked = false;
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                m_cursorIsLocked = true;
+            }
+
+            if (m_cursorIsLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else if (!m_cursorIsLocked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+>>>>>>> parent of 855f29b... 3D应急演练系统 0.1.5:Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
         Quaternion ClampRotationAroundXAxis(Quaternion q)
         {
             q.x /= q.w;
